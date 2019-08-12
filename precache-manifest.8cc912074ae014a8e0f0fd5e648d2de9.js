83370db4d1e553d0494e8ccda2daa66f33f46d67
Greyti <47736158+Greyti8888@users.noreply.github.com>
2019-08-12 11:33:06 +0300

    Updates

---
+++ precache-manifest.8cc912074ae014a8e0f0fd5e648d2de9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d0f24cc1fd3f9a0140a502aed05f99",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "b09490dfb6a3221619a2",
+    "url": "/random-quote-machine/static/js/2.0a76d362.chunk.js"
+  },
+  {
+    "revision": "e861eec881eb38391ac0",
+    "url": "/random-quote-machine/static/js/main.d6dbd2dc.chunk.js"
+  },
+  {
+    "revision": "058414b773158bd069be",
+    "url": "/random-quote-machine/static/js/runtime~main.c254d928.js"
+  }
+]);
